fdde73fc2fb990a11c7eb79abc55bd74af306a36
Rolland Safort <safort@era-app.cn>
2016-04-18 20:16:54 +0800

    login new company

--- public/app/components/settings/access/settings-access.component.js
+++ public/app/components/settings/access/settings-access.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', '../../../config'], function(exports_1, context_1) {
+System.register(['angular2/core', '../../../config', './partials/settings-add-user.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', '../../../config'], function(exports_1, contex
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, config_1;
+    var core_1, config_1, settings_add_user_component_1;
     var SettingsAccess;
     return {
         setters:[
@@ -19,16 +19,168 @@ System.register(['angular2/core', '../../../config'], function(exports_1, contex
             },
             function (config_1_1) {
                 config_1 = config_1_1;
+            },
+            function (settings_add_user_component_1_1) {
+                settings_add_user_component_1 = settings_add_user_component_1_1;
             }],
         execute: function() {
             SettingsAccess = (function () {
                 function SettingsAccess() {
+                    this.alertsList = [
+                        {
+                            name: 'C002-闸北区大宁路335号XX站',
+                            id: '6848',
+                            type: '余量报警',
+                            remainingTime: '2小时02分',
+                            upTime: '15.5.3-13:02/----',
+                            processed: false,
+                            alertTime: '5.5.3-13:02',
+                            alertValue: '6%/12kg/hps'
+                        },
+                        {
+                            name: 'C003-闸北区大宁路335号XX站',
+                            id: '6848',
+                            type: '余量报警',
+                            remainingTime: '2小时02分',
+                            upTime: '15.5.3-13:02/----',
+                            processed: true,
+                            alertTime: '5.5.3-13:02',
+                            alertValue: '6%/12kg/hps'
+                        },
+                        {
+                            name: 'C004-闸北区大宁路335号XX站',
+                            id: '6832',
+                            type: '信号中断',
+                            remainingTime: '',
+                            upTime: '15.5.3-13:02/----',
+                            processed: false,
+                            alertTime: '5.5.3-13:02',
+                            alertValue: '信号中断'
+                        },
+                        {
+                            name: 'C005-闸北区大宁路335号XX站',
+                            id: '6832',
+                            type: '信号中断',
+                            remainingTime: '',
+                            upTime: '15.5.3-13:02/----',
+                            processed: true,
+                            alertTime: '5.5.3-13:02',
+                            alertValue: '信号中断'
+                        },
+                        {
+                            name: 'C006-闸北区大宁路335号XX站',
+                            id: '6832',
+                            type: '信号中断',
+                            remainingTime: '',
+                            upTime: '15.5.3-13:02/----',
+                            processed: false,
+                            alertTime: '5.5.3-13:02',
+                            alertValue: '信号中断'
+                        },
+                        {
+                            name: 'C007-闸北区大宁路335号XX站',
+                            id: '6832',
+                            type: '信号中断',
+                            remainingTime: '',
+                            upTime: '15.5.3-13:02/----',
+                            processed: false,
+                            alertTime: '5.5.3-13:02',
+                            alertValue: '信号中断'
+                        },
+                        {
+                            name: 'C007-闸北区大宁路335号XX站',
+                            id: '6832',
+                            type: '泄漏报警',
+                            remainingTime: '',
+                            upTime: '15.5.3-13:02/----',
+                            processed: false,
+                            alertTime: '5.5.3-13:02',
+                            alertValue: '泄漏报警'
+                        },
+                        {
+                            name: 'C007-闸北区大宁路335号XX站',
+                            id: '6832',
+                            type: '泄漏报警',
+                            remainingTime: '',
+                            upTime: '15.5.3-13:02/----',
+                            processed: false,
+                            alertTime: '5.5.3-13:02',
+                            alertValue: '泄漏报警'
+                        },
+                        {
+                            name: 'C007-闸北区大宁路335号XX站',
+                            id: '6832',
+                            type: '压力报警',
+                            remainingTime: '',
+                            upTime: '15.5.3-13:02/----',
+                            processed: false,
+                            alertTime: '5.5.3-13:02',
+                            alertValue: '压力报警'
+                        },
+                        {
+                            name: 'C007-闸北区大宁路335号XX站',
+                            id: '6832',
+                            type: '压力报警',
+                            remainingTime: '',
+                            upTime: '15.5.3-13:02/----',
+                            processed: true,
+                            alertTime: '5.5.3-13:02',
+                            alertValue: '压力报警'
+                        }
+                    ]; //todo user flag and ng if to hide when filtering;
+                    this.currentSort = 'all';
                     console.log("SettingsAccess is up and running");
+                    this.initUi();
                 }
+                SettingsAccess.prototype.veSortByShortage = function () {
+                    if (this.currentSort != '余量报警') {
+                        this.currentSort = '余量报警';
+                    }
+                };
+                SettingsAccess.prototype.veSortBySingal = function () {
+                    if (this.currentSort != '信号中断') {
+                        this.currentSort = '信号中断';
+                    }
+                };
+                SettingsAccess.prototype.veSortByPresure = function () {
+                    if (this.currentSort != '压力报警') {
+                        this.currentSort = '压力报警';
+                    }
+                };
+                SettingsAccess.prototype.veSortByLeakage = function () {
+                    if (this.currentSort != '泄漏报警') {
+                        this.currentSort = '泄漏报警';
+                    }
+                };
+                SettingsAccess.prototype.veProcessed = function (alert) {
+                    alert.processed = !alert.processed;
+                };
+                SettingsAccess.prototype.initUi = function () {
+                    var _this = this;
+                    setTimeout(function (_) {
+                        jQuery('.modal-trigger').leanModal({
+                            dismissible: true,
+                            opacity: .5,
+                            in_duration: 300,
+                            out_duration: 200,
+                            ready: function () { console.log('Ready'); _this.initSelect(); },
+                            complete: function () { console.log('Closed'); } // Callback for Modal close
+                        });
+                        jQuery('.collapsible').collapsible({
+                            accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+                        });
+                    });
+                };
+                SettingsAccess.prototype.initSelect = function () {
+                    setTimeout(function (_) {
+                        jQuery('select').material_select();
+                    });
+                };
                 SettingsAccess = __decorate([
                     core_1.Component({
                         selector: 'settings-access',
-                        templateUrl: config_1.config.prefix + '/components/settings/access/settings-access.component.html'
+                        templateUrl: config_1.config.prefix + '/components/settings/access/settings-access.component.html',
+                        directives: [settings_add_user_component_1.SettingsAddUser]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], SettingsAccess);
--- public/app/components/settings/access/settings-access.component.ts
+++ public/app/components/settings/access/settings-access.component.ts
@@ -1,16 +1,180 @@
 import {Component, provide} from 'angular2/core';
 import {config} from '../../../config';
+import {SettingsAddUser} from './partials/settings-add-user.component';
 declare var jQuery:any;
 
 @Component({
   selector:'settings-access',
-  templateUrl:config.prefix + '/components/settings/access/settings-access.component.html'
+  templateUrl:config.prefix + '/components/settings/access/settings-access.component.html',
+  directives:[SettingsAddUser]
 })
 
 export class SettingsAccess{
+
+  alertsList:any[]=[
+      {
+        name:'C002-闸北区大宁路335号XX站',
+        id:'6848',
+        type:'余量报警',
+        remainingTime:'2小时02分',
+        upTime:'15.5.3-13:02/----',
+        processed:false,
+        alertTime:'5.5.3-13:02',
+        alertValue:'6%/12kg/hps'
+      },
+      {
+        name:'C003-闸北区大宁路335号XX站',
+        id:'6848',
+        type:'余量报警',
+        remainingTime:'2小时02分',
+        upTime:'15.5.3-13:02/----',
+        processed:true,
+        alertTime:'5.5.3-13:02',
+        alertValue:'6%/12kg/hps'
+      },
+      {
+        name:'C004-闸北区大宁路335号XX站',
+        id:'6832',
+        type:'信号中断',
+        remainingTime:'',
+        upTime:'15.5.3-13:02/----',
+        processed:false,
+        alertTime:'5.5.3-13:02',
+        alertValue:'信号中断'
+      }
+      ,  {
+          name:'C005-闸北区大宁路335号XX站',
+          id:'6832',
+          type:'信号中断',
+          remainingTime:'',
+          upTime:'15.5.3-13:02/----',
+          processed:true,
+          alertTime:'5.5.3-13:02',
+          alertValue:'信号中断'
+        },
+        {
+          name:'C006-闸北区大宁路335号XX站',
+          id:'6832',
+          type:'信号中断',
+          remainingTime:'',
+          upTime:'15.5.3-13:02/----',
+          processed:false,
+          alertTime:'5.5.3-13:02',
+          alertValue:'信号中断'
+        },
+        {
+          name:'C007-闸北区大宁路335号XX站',
+          id:'6832',
+          type:'信号中断',
+          remainingTime:'',
+          upTime:'15.5.3-13:02/----',
+          processed:false,
+          alertTime:'5.5.3-13:02',
+          alertValue:'信号中断'
+        },
+        {
+          name:'C007-闸北区大宁路335号XX站',
+          id:'6832',
+          type:'泄漏报警',
+          remainingTime:'',
+          upTime:'15.5.3-13:02/----',
+          processed:false,
+          alertTime:'5.5.3-13:02',
+          alertValue:'泄漏报警'
+        },
+        {
+          name:'C007-闸北区大宁路335号XX站',
+          id:'6832',
+          type:'泄漏报警',
+          remainingTime:'',
+          upTime:'15.5.3-13:02/----',
+          processed:false,
+          alertTime:'5.5.3-13:02',
+          alertValue:'泄漏报警'
+        },
+        {
+          name:'C007-闸北区大宁路335号XX站',
+          id:'6832',
+          type:'压力报警',
+          remainingTime:'',
+          upTime:'15.5.3-13:02/----',
+          processed:false,
+          alertTime:'5.5.3-13:02',
+          alertValue:'压力报警'
+        },
+        {
+          name:'C007-闸北区大宁路335号XX站',
+          id:'6832',
+          type:'压力报警',
+          remainingTime:'',
+          upTime:'15.5.3-13:02/----',
+          processed:true,
+          alertTime:'5.5.3-13:02',
+          alertValue:'压力报警'
+        }
+
+
+  ];  //todo user flag and ng if to hide when filtering;
+
+  currentSort:string = 'all';
+
     constructor(){
       console.log("SettingsAccess is up and running");
 
+      this.initUi();
     }
 
+    veSortByShortage(){
+      if(this.currentSort!='余量报警'){
+        this.currentSort='余量报警';
+      }
+    }
+
+    veSortBySingal(){
+      if(this.currentSort!='信号中断'){
+        this.currentSort='信号中断';
+      }
+    }
+
+    veSortByPresure(){
+      if(this.currentSort!='压力报警'){
+        this.currentSort='压力报警';
+      }
+    }
+
+    veSortByLeakage(){
+      if(this.currentSort!='泄漏报警'){
+        this.currentSort='泄漏报警';
+      }
+    }
+
+    veProcessed(alert){
+
+      alert.processed = !alert.processed;
+
+    }
+    initUi(){
+
+      var _this = this;
+
+        setTimeout(_=>{
+            jQuery('.modal-trigger').leanModal({
+                 dismissible: true, // Modal can be dismissed by clicking outside of the modal
+                 opacity: .5, // Opacity of modal background
+                 in_duration: 300, // Transition in duration
+                 out_duration: 200, // Transition out duration
+                 ready: function() { console.log('Ready');  _this.initSelect()}, // Callback for Modal open
+                 complete: function() { console.log('Closed'); } // Callback for Modal close
+           });
+           jQuery('.collapsible').collapsible({
+             accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+           });
+        });
+    }
+
+    initSelect(){
+      setTimeout(_=>{
+           jQuery('select').material_select();
+      });
+    }
  }
