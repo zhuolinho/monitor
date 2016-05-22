b14df6c05e2273b66b8832e6cbfac71e51859853
Rolland Safort <safort@era-app.cn>
2016-05-22 16:21:02 +0800

    update

--- app_modules/sms/sms.module.js
+++ app_modules/sms/sms.module.js
@@ -1,27 +1,27 @@
 'use strict';
 
-var yunpainSMSClient = require('yunpian-sms-client');
+// var yunpainSMSClient = require('yunpian-sms-client');
 
 var sms = {};
 
 
-sms.send = function(){
-
-  var c = new yunpainSMSClient({
-      apiKey: '3228b665719a882665f814c29374f002',
-      sendContent: '【优载】Hello 这是测试短信 ',
-      mobile: ['your number']  //注意：请自行验证手机号码是否正确
-  });
-
-
-  c.sendSMS(function (err, result) {
-      if (err) {
-          console.log('sms error',err);
-          return;
-      }
-      console.log('sms sent',result);
-  });
-}
+// sms.send = function(){
+//
+//   var c = new yunpainSMSClient({
+//       apiKey: '3228b665719a882665f814c29374f002',
+//       sendContent: '【优载】Hello 这是测试短信 ',
+//       mobile: ['your number']  //注意：请自行验证手机号码是否正确
+//   });
+//
+//
+//   c.sendSMS(function (err, result) {
+//       if (err) {
+//           console.log('sms error',err);
+//           return;
+//       }
+//       console.log('sms sent',result);
+//   });
+// }
 
 
 
