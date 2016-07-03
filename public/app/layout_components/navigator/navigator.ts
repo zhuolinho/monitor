4cf75689cd5946e9101c13c6430a99dca60ee208
zhuolinho <zhuolinho@hotmail.com>
2016-07-03 15:38:19 +0800

    navigator button

--- public/app/layout_components/navigator/navigator.ts
+++ public/app/layout_components/navigator/navigator.ts
@@ -13,6 +13,8 @@ declare var jQuery:any;
 
 export class Navigator {
    navigations:any[];
+   menu:number=0;
+   url:string="#/admin/monitor";
    constructor() {
       this.navigations = [
           {link:['/Admin','Home'], title:'首页', icon:'dashboard'},
@@ -20,6 +22,15 @@ export class Navigator {
           {link:['/Admin','Gps'], title:'GPS ', icon:'my_location'},
           {link:['/Admin','Settings'], title:'设置 ', icon:'settings'}
         ]
-
+    }
+    shiftMenu() {
+      this.menu++;
+      if (this.menu % 3 == 0) {
+        this.url = "#/admin/monitor";
+      } else if (this.menu % 3 == 1) {
+        this.url = "#/admin/gps";
+      } else if (this.menu % 3 == 2) {
+        this.url = "#/admin/home";
       }
+    }
 }
