da1219226220d80e8679e21848f38b2712689261
Rolland Safort <safort@era-app.cn>
2016-07-11 13:28:03 +0800

    update flyplan

---
+++ flightplan.js
@@ -0,0 +1,26 @@
+var plan = require('flightplan');
+
+var appName = 'monitor';
+var username = 'webmaster';
+
+// configuration
+plan.target('production', [
+  {
+    host: '139.196.18.222',
+    username: username,
+    agent: process.env.SSH_AUTH_SOCK
+  }
+]);
+// run commands on localhost
+plan.local(function(local) {
+  local.log('Copy files to remote hosts');
+  var filesToCopy = local.exec('git ls-files', {exec: {maxBuffer: 10000*1024}});
+  // rsync files to all the destination's hosts
+  local.transfer(filesToCopy, '~/monitor');
+});
+
+// run commands on remote hosts (destinations)
+plan.remote(function(remote) {
+  remote.log('Reload application');
+  remote.exec('pm2 reload '+appName , {user: username});
+});
