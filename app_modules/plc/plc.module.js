6da20bd8c8f2c7efc560ffac9975fa6b31e58a5b
Rolland Safort <rollandsafort@163.com>
2016-11-27 13:23:43 +0800

    alert triggering condition reenforced

--- app_modules/plc/plc.module.js
+++ app_modules/plc/plc.module.js
@@ -1316,33 +1316,34 @@ var _getPlcType = function(data,shift){
 var _checkChanelInterruption = function(data, oID,latestIncommingData,latestInteruptedChanels){
     var result = {createAlert:false,save:false, latestIncommingData:latestIncommingData,latestInteruptedChanels:latestInteruptedChanels};
 
-    if(latestInteruptedChanels[oID]){  //is always set by default
-          if (!latestInteruptedChanels[oID][data.tank]){ //make sure the interuption has not been registered yet
-            if (latestIncommingData[oID][data.tank]){//if not the first time to get this data
-                if ((latestIncommingData[oID][data.tank].cdct+latestIncommingData[oID][data.tank].cdcns) === (data.cdct+data.cdcns)){  //if same as previous value
-                    latestInteruptedChanels[oID][data.tank] = data;
-                    //keep to create interuption alert
-                    result.createAlert = true;
+          if(latestInteruptedChanels[oID]){  //is always set by default
+                if (!latestInteruptedChanels[oID][data.tank]){ //make sure the interuption has not been registered yet
+                  if (latestIncommingData[oID][data.tank]){//if not the first time to get this data
+                      if ((latestIncommingData[oID][data.tank].cdct+latestIncommingData[oID][data.tank].cdcns) === (data.cdct+data.cdcns)){  //if same as previous value
+                            if(Math.abs(data.dct-data.cdct)>(plcConfig.sTimer/2)){   //check difference betwen sample and transmition times.
+                                latestInteruptedChanels[oID][data.tank] = data;
+                                //keep to create interuption alert
+                                result.createAlert = true;
+                            }
+                      }
+                      else{
+                        result.save = true;
+                        latestIncommingData[oID][data.tank] = data; //update latest data
+                      }
+                  }
+                  else{
+                    result.save = true;
+                    latestIncommingData[oID][data.tank] = data; //save in latest data
+                  }
                 }
-                else{
-                  result.save = true;
-                  latestIncommingData[oID][data.tank] = data; //update latest data
+                else {
+                    if ((latestIncommingData[oID][data.tank].cdct+latestIncommingData[oID][data.tank].cdcns) !== (data.cdct+data.cdcns)){  //normal flow restored
+                        delete latestInteruptedChanels[oID][data.tank];
+                        result.save = true;
+                        latestIncommingData[oID][data.tank] = data;  //update latest data
+                    }
                 }
-            }
-            else{
-              result.save = true;
-              latestIncommingData[oID][data.tank] = data; //save in latest data
-            }
-          }
-          else {
-              if ((latestIncommingData[oID][data.tank].cdct+latestIncommingData[oID][data.tank].cdcns) !== (data.cdct+data.cdcns)){  //normal flow restored
-                  delete latestInteruptedChanels[oID][data.tank];
-                  result.save = true;
-                  latestIncommingData[oID][data.tank] = data;  //update latest data
-              }
-          }
     }
-
     return result;
 }
 
