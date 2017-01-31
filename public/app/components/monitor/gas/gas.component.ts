42b1ae3fede475c545020bba11fc68b628a2bbe5
rolland safort <tamchat_dev@163.com>
2017-01-31 22:07:54 +0800

    updated chart

--- public/app/components/monitor/gas/gas.component.ts
+++ public/app/components/monitor/gas/gas.component.ts
@@ -658,8 +658,9 @@ export class Gas  implements AfterViewInit,OnDestroy{
                     type: 'timeseries',
                      tick: {
                          count: 24,
+                          values: [1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
                         //  format: function (x) { return x.getFullYear(); }
-                         format:'%H:%M' //how the date is displayed
+                        //  format:'%H:%M' //how the date is displayed
                      }
                   }
               }
