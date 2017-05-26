56b48d7e920c7afeb18b45fc00c5416eb5f39b98
rolland safort <tamchat_dev@163.com>
2017-05-26 09:09:51 +0800

    gas: sorting plc by number in drop drop down select

--- public/app/components/monitor/gas/gas.component.ts
+++ public/app/components/monitor/gas/gas.component.ts
@@ -20,247 +20,6 @@ declare var c3:any;
 
 export class Gas  implements AfterViewInit,OnDestroy{
 
-  testPlcs = [
-    {
-      "_id": "5810d7cfa7d1a71e69621e6b",
-      "muID": "system",
-      "cuID": "system",
-      "oID": "10000000001",
-      "cd": "2016-10-27 00:20:31",
-      "y": "2016",
-      "m": "10",
-      "d": "27",
-      "dct": "2016-10-27 0:5:2",
-      "cdct": "2016-10-27 0:5:23",
-      "addr1": "0",
-      "iwc1": "0",
-      "isc1": "0",
-      "p1": "0",
-      "temp1": "0",
-      "pwc1": "0",
-      "psc1": "0",
-      "rsc1": "0",
-      "cf1": "1",
-      "er1": "0",
-      "addr2": "3",
-      "iwc2": "13.8",
-      "isc2": "55.1",
-      "p2": "398.1",
-      "temp2": "17.5",
-      "pwc2": "164633.0",
-      "psc2": "2522930.0",
-      "rsc2": "61.0",
-      "cf2": "0",
-      "er2": "0",
-      "tank": "L001",
-      "__v": 0
-    },
-    {
-      "_id": "5810d7cfa7d1a71e69621e6c",
-      "muID": "system",
-      "cuID": "system",
-      "oID": "10000000001",
-      "cd": "2016-10-27 00:20:31",
-      "y": "2016",
-      "m": "10",
-      "d": "27",
-      "dct": "2016-10-27 0:5:2",
-      "cdct": "2016-10-27 0:4:32",
-      "addr1": "2",
-      "iwc1": "0",
-      "isc1": "0",
-      "p1": "365.7",
-      "temp1": "22.6",
-      "pwc1": "346385.0",
-      "psc1": "6374181.0",
-      "rsc1": "653.0",
-      "cf1": "0",
-      "er1": "0",
-      "addr2": "0",
-      "iwc2": "0",
-      "isc2": "0",
-      "p2": "0",
-      "temp2": "0",
-      "pwc2": "0",
-      "psc2": "0",
-      "rsc2": "0",
-      "cf2": "1",
-      "er2": "0",
-      "tank": "L002",
-      "__v": 0
-    },
-    {
-      "_id": "5810d7cfa7d1a71e69621e6d",
-      "muID": "system",
-      "cuID": "system",
-      "oID": "10000000001",
-      "cd": "2016-10-27 00:20:31",
-      "y": "2016",
-      "m": "10",
-      "d": "27",
-      "dct": "2016-10-27 0:5:2",
-      "cdct": "2016-10-27 0:5:27",
-      "addr1": "2",
-      "iwc1": "28.2",
-      "isc1": "107",
-      "p1": "372.0",
-      "temp1": "23.2",
-      "pwc1": "37889.0",
-      "psc1": "1649238.0",
-      "rsc1": "0",
-      "cf1": "0",
-      "er1": "0",
-      "addr2": "0",
-      "iwc2": "0",
-      "isc2": "0",
-      "p2": "0",
-      "temp2": "0",
-      "pwc2": "0",
-      "psc2": "0",
-      "rsc2": "0",
-      "cf2": "1",
-      "er2": "0",
-      "tank": "L003",
-      "__v": 0
-    },
-    {
-      "_id": "5810d7cfa7d1a71e69621e6e",
-      "muID": "system",
-      "cuID": "system",
-      "oID": "10000000001",
-      "cd": "2016-10-27 00:20:31",
-      "y": "2016",
-      "m": "10",
-      "d": "27",
-      "dct": "2016-10-27 0:5:2",
-      "cdct": "2016-10-26 11:26:9",
-      "addr1": "0",
-      "iwc1": "0",
-      "isc1": "0",
-      "p1": "0",
-      "temp1": "0",
-      "pwc1": "0",
-      "psc1": "0",
-      "rsc1": "0",
-      "cf1": "1",
-      "er1": "0",
-      "addr2": "0",
-      "iwc2": "0",
-      "isc2": "0",
-      "p2": "0",
-      "temp2": "0",
-      "pwc2": "0",
-      "psc2": "0",
-      "rsc2": "0",
-      "cf2": "1",
-      "er2": "0",
-      "tank": "L004",
-      "__v": 0
-    },
-    {
-      "_id": "5810d7cfa7d1a71e69621e6f",
-      "muID": "system",
-      "cuID": "system",
-      "oID": "10000000001",
-      "cd": "2016-10-27 00:20:31",
-      "y": "2016",
-      "m": "10",
-      "d": "27",
-      "dct": "2016-10-27 0:5:2",
-      "cdct": "2016-10-27 0:5:10",
-      "addr1": "2",
-      "iwc1": "83.5",
-      "isc1": "298",
-      "p1": "362.9",
-      "temp1": "21.1",
-      "pwc1": "605653.0",
-      "psc1": "12117552.0",
-      "rsc1": "545.0",
-      "cf1": "0",
-      "er1": "0",
-      "addr2": "3",
-      "iwc2": "0",
-      "isc2": "0",
-      "p2": "97.9",
-      "temp2": "20.3",
-      "pwc2": "0",
-      "psc2": "141222.0",
-      "rsc2": "0",
-      "cf2": "0",
-      "er2": "0",
-      "tank": "L005",
-      "__v": 0
-    },
-    {
-      "_id": "5810d7cfa7d1a71e69621e78",
-      "muID": "system",
-      "cuID": "system",
-      "oID": "10000000001",
-      "cd": "2016-10-27 00:20:31",
-      "y": "2016",
-      "m": "10",
-      "d": "27",
-      "dct": "2016-10-27 0:5:2",
-      "cdct": "2016-10-26 14:51:52",
-      "addr1": "0",
-      "iwc1": "0",
-      "isc1": "0",
-      "p1": "0",
-      "temp1": "0",
-      "pwc1": "0",
-      "psc1": "0",
-      "rsc1": "0",
-      "cf1": "1",
-      "er1": "0",
-      "addr2": "0",
-      "iwc2": "0",
-      "isc2": "0",
-      "p2": "0",
-      "temp2": "0",
-      "pwc2": "0",
-      "psc2": "0",
-      "rsc2": "0",
-      "cf2": "1",
-      "er2": "0",
-      "tank": "L014",
-      "__v": 0
-    },
-    {
-      "_id": "5810d7cfa7d1a71e69621e7b",
-      "muID": "system",
-      "cuID": "system",
-      "oID": "10000000001",
-      "cd": "2016-10-27 00:20:31",
-      "y": "2016",
-      "m": "10",
-      "d": "27",
-      "dct": "2016-10-27 0:5:2",
-      "cdct": "2016-10-27 0:5:19",
-      "addr1": "2",
-      "iwc1": "28.0",
-      "isc1": "103",
-      "p1": "372.6",
-      "temp1": "22.9",
-      "pwc1": "460190.0",
-      "psc1": "7104624.0",
-      "rsc1": "0",
-      "cf1": "0",
-      "er1": "0",
-      "addr2": "3",
-      "iwc2": "0",
-      "isc2": "0",
-      "p2": "94.6",
-      "temp2": "22.8",
-      "pwc2": "22693.0",
-      "psc2": "440.0",
-      "rsc2": "334.0",
-      "cf2": "0",
-      "er2": "0",
-      "tank": "L017",
-      "__v": 0
-    }
-  ];
-
   months:number[] = [1,2,3,4,5,6,7,8,9,10,11,12];
   years:number[] = [];
   startDays:number[] = [];
@@ -338,12 +97,11 @@ export class Gas  implements AfterViewInit,OnDestroy{
       this.request.get('/plc/latest/withaddress.json').subscribe(resp => {
         console.log("latest plc>>>-----",resp);
         if(resp&&resp.pl&&resp.pl.plc&&resp.pl.address){
-
-            // this.realTimeData = _.keyBy(resp.pl.plc,'tank');
             this.realTimeData = resp.pl.plc;
             this.plcAddresses = _.keyBy(resp.pl.address,'tank');
-            // this.realTimeData = _.keyBy(this.testPlcs,'tank');
-            this.connectedPlcs = Object.keys(this.realTimeData);
+            this.connectedPlcs = _.sortBy(Object.keys(this.realTimeData), (o) => {
+              return parseInt(o.slice(1, 4));
+            });
             this.currentPlcTank = this.connectedPlcs[0];
             this.initSelect();
         }
@@ -540,8 +298,6 @@ export class Gas  implements AfterViewInit,OnDestroy{
     }
 
     setStatsInitValues(){
-
-
       var d = new Date();
       this.statsEndDate = d.toISOString().slice(0,10);
       d.setMonth(d.getMonth() - 1);//last month date;
@@ -576,7 +332,6 @@ export class Gas  implements AfterViewInit,OnDestroy{
 
 
     downloadData(){
-
         var which = '';
         var mode = null;
 
@@ -591,7 +346,6 @@ export class Gas  implements AfterViewInit,OnDestroy{
             which = 'monthly-usage';
             mode = 'month';
         }
-
 
         this.request.post('/plc/stats/download.json',{start:this.statsStartDate,end:this.statsEndDate,which:which,mode:mode,tank:this.currentPlcTank}).subscribe(res => {
           console.log("res-----",res);
